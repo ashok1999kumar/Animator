f399d52f34911b5b1bea690030e94c00a5832a76
ashokkumar <ak9885129@gmail.com>
2021-12-02 18:10:40 +0530

    navbar created

--- flex/src/Components/Nav.jsx
+++ flex/src/Components/Nav.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 export const Nav = () => {
     return (
-        <div>
-            
+        <div style={{display:"flex",padding:"1rem",
+        
+        }}>
+           <div style={{flex:13, borderRadius:"50%",width:"2rem", height:"2rem"}}>
+            circle
+           </div>
+           <div style={{flex:5}}>
+                <ul style={{display:"flex", listStyle:"none", justifyContent:"space-between"}}>
+                    <li><a href="#" style={{textDecoration:"none"}}>One</a></li>
+                    <li><a href="#" style={{textDecoration:"none"}}>Two</a></li>
+                    <li><a href="#" style={{textDecoration:"none"}}>Three</a></li>
+                    <li><a href="#" style={{textDecoration:"none"}}>Four</a></li>
+                </ul>
+           </div> 
         </div>
     )
 }
